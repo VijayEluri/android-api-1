94e1d9667a2f8c843236824babf9a81afc109361
Rodja Trappe <rodja@hoccer.com>
2010-12-02 12:02:23 +0100

    skeleton for async linccer

---
+++ src/com/hoccer/api/android/AsyncLinccer.java
@@ -0,0 +1,12 @@
+package com.hoccer.api.android;
+
+import android.os.*;
+
+public class AsyncLinccer extends AsyncTask<Object, Object, Object> {
+
+    @Override
+    protected Object doInBackground(Object... params) {
+        return null;
+    }
+
+}

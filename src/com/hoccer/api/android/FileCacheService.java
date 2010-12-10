732c040298f2ba56f6e2811f1bda241930884d44
Rodja Trappe <rodja@hoccer.com>
2010-12-10 12:07:45 +0100

    added store method

--- src/com/hoccer/api/android/FileCacheService.java
+++ src/com/hoccer/api/android/FileCacheService.java
@@ -1,13 +1,16 @@
 package com.hoccer.api.android;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import com.hoccer.api.ClientConfig;
 import com.hoccer.data.StreamableContent;
 import com.hoccer.http.AsyncHttpGet;
+import com.hoccer.http.AsyncHttpPut;
 import com.hoccer.http.AsyncHttpRequest;
 import com.hoccer.http.HttpResponseHandler;
 
@@ -25,6 +28,20 @@ public class FileCacheService extends Service {
         fetchRequest.setStreamableContent(sink);
         fetchRequest.start();
         mOngoingRequests.put(url, fetchRequest);
+    }
+
+    public String store(StreamableContent source, int secondsUntilExipred,
+            HttpResponseHandler responseHandler) throws IOException {
+        String url = ClientConfig.getFileCacheBaseUri() + "/" + source.getFilename()
+                + "/?expires_in=" + secondsUntilExipred;
+
+        AsyncHttpPut storeRequest = new AsyncHttpPut(url);
+        storeRequest.registerResponseHandler(responseHandler);
+        storeRequest.setBody(source);
+        storeRequest.start();
+        mOngoingRequests.put(url, storeRequest);
+
+        return url;
     }
 
     @Override

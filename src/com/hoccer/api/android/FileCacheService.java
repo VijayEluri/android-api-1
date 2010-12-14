fef7cdfcd93b013d1d2583d3ca70664f2efb6f09
Rodja Trappe <rodja@hoccer.com>
2010-12-14 16:47:29 +0100

    using asyncStore from FileCache

--- src/com/hoccer/api/android/FileCacheService.java
+++ src/com/hoccer/api/android/FileCacheService.java
@@ -1,26 +1,27 @@
 package com.hoccer.api.android;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
 
 import com.hoccer.api.ClientConfig;
+import com.hoccer.api.FileCache;
 import com.hoccer.data.StreamableContent;
 import com.hoccer.http.AsyncHttpGet;
-import com.hoccer.http.AsyncHttpPut;
-import com.hoccer.http.AsyncHttpRequest;
 import com.hoccer.http.HttpResponseHandler;
 
 public class FileCacheService extends Service {
 
-    private final HashMap<String, AsyncHttpRequest> mOngoingRequests = new HashMap<String, AsyncHttpRequest>();
+    private FileCache mFileCache;
+
+    public void setConfig(ClientConfig config) {
+        mFileCache = new FileCache(config);
+    }
 
     protected void stopWhenAllLoadsFinished() {
-        if (mOngoingRequests.size() == 0) {
+        if (mFileCache.getOngoingRequests().size() == 0) {
             stopSelf();
         }
     }
@@ -30,29 +31,15 @@ public class FileCacheService extends Service {
         fetchRequest.registerResponseHandler(responseHandler);
         fetchRequest.setStreamableContent(sink);
         fetchRequest.start();
-        mOngoingRequests.put(uri, fetchRequest);
     }
 
     public String store(StreamableContent source, int secondsUntilExipred,
             HttpResponseHandler responseHandler) throws IOException {
-        String uri = ClientConfig.getFileCacheBaseUri() + "/" + source.getFilename()
-                + "?expires_in=" + secondsUntilExipred;
-
-        AsyncHttpPut storeRequest = new AsyncHttpPut(uri);
-        storeRequest.registerResponseHandler(responseHandler);
-        storeRequest.setBody(source);
-        storeRequest.start();
-        mOngoingRequests.put(uri, storeRequest);
-
-        return uri;
+        return mFileCache.asyncStore(source, secondsUntilExipred, responseHandler);
     }
 
     public void cancel(String uri) {
-        AsyncHttpRequest request = mOngoingRequests.remove(uri);
-        request.interrupt();
-        Log.v("FileCacheService", "aborting " + uri + " as " + request + " interr: "
-                + request.isInterrupted());
-        ;
+        mFileCache.cancel(uri);
     }
 
     @Override

d8fb53077cd97a0ecd3324753ddeabc52a8b8387
Arne Handt <it@handtwerk.de>
2012-03-19 17:24:57 +0100

    added AndroidClientConfig

---
+++ src/com/hoccer/api/android/AndroidClientConfig.java
@@ -0,0 +1,90 @@
+package com.hoccer.api.android;
+
+import java.util.UUID;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.hoccer.api.ClientConfig;
+
+public class AndroidClientConfig extends ClientConfig {
+
+    // Static Methods ----------------------------------------------------
+
+    public static String getServerNameFromSharedPreferences(Context context) {
+
+        SharedPreferences prefs = context.getSharedPreferences("com.artcom.hoccer_preferences",
+                Context.MODE_WORLD_READABLE | Context.MODE_WORLD_WRITEABLE);
+
+        String tmpServerName = "https://linccer.hoccer.com/v3";
+        String serverName = prefs.getString("hoccer_server", tmpServerName);
+
+        if (tmpServerName.equals(serverName)) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("hoccer_server", tmpServerName);
+            editor.commit();
+        }
+        return serverName;
+    }
+
+    public static String getFileCacheServerNameFromSharedPreferences(Context context) {
+
+        SharedPreferences prefs = context.getSharedPreferences("com.artcom.hoccer_preferences",
+                Context.MODE_WORLD_READABLE | Context.MODE_WORLD_WRITEABLE);
+
+        String tmpServerName = "https://filecache.hoccer.com/v3";
+        String serverName = prefs.getString("hoccer_filecache_server", tmpServerName);
+
+        if (tmpServerName.equals(serverName)) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("hoccer_filecache_server", tmpServerName);
+            editor.commit();
+        }
+        return serverName;
+    }
+
+    public static String getHocletServerNameFromSharedPreferences(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("com.artcom.hoccer_preferences",
+                Context.MODE_WORLD_READABLE | Context.MODE_WORLD_WRITEABLE);
+
+        String tmpServerName = "https://hoclet-experimental.hoccer.com/v3";
+        String serverName = prefs.getString("hoccer_hoclet_server", tmpServerName);
+
+        if (tmpServerName.equals(serverName)) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("hoccer_hoclet_server", tmpServerName);
+            editor.commit();
+        }
+        return serverName;
+    }
+
+    public static void useSettingsServers(Context context) {
+
+        mLinccerUri = getServerNameFromSharedPreferences(context);
+        mFileCacheUri = getFileCacheServerNameFromSharedPreferences(context);
+    }
+
+    // Constructors ------------------------------------------------------
+
+    public AndroidClientConfig(String applicatioName, String apiKey, String sharedSecret, UUID clientId,
+            String clientName, String publicKey) {
+
+        super(applicatioName, apiKey, sharedSecret, clientId, clientName, publicKey);
+    }
+
+    public AndroidClientConfig(String applicatioName, String apiKey, String sharedSecret) {
+
+        super(applicatioName, apiKey, sharedSecret);
+    }
+
+    public AndroidClientConfig(String applicatioName, UUID clientId, String clientName) {
+
+        super(applicatioName, clientId, clientName);
+    }
+
+    public AndroidClientConfig(String applicatioName) {
+
+        super(applicatioName);
+    }
+
+}
